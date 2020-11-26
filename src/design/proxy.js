cc2c86641546842c6a9e9697e52a4daf8cd57eef
linyuqing <linyuqing@jomoo.cn>
2020-11-26 13:44:58 +0800

    代理模式，迭代器模式

---
+++ src/design/proxy.js
@@ -0,0 +1,23 @@
+let mult = function () {
+  console.log("我在计算！");
+  let a = 1;
+  for (let i = 0, l = arguments.length; i < l; i++) {
+    a = a * arguments[i];
+  }
+  return a;
+};
+
+let proxyMult = (function () {
+  let cache = {};
+  return function () {
+    let args = Array.prototype.join.call(arguments, ",");
+    // 根据参数的拼接方式决定属性名称，1,2,3
+    // 使用 in 会从对象或者原型中查找
+    if (args in cache) {
+      return cache[args];
+    }
+    return (cache[args] = mult.apply(this, arguments));
+  };
+})();
+console.log(proxyMult(1, 2, 3));
+console.log(proxyMult(1, 2, 3));
